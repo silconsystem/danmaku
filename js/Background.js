3dfb8267ddefed0bd3a4db7d62a2c746535cdfa1
silconsystem <lysergic_evangelion@live.nl>
2012-09-08 21:33:56 +0200

    stage1-2 backgrounds

--- js/Background.js
+++ js/Background.js
@@ -13,36 +13,46 @@ function Background()
 	this.w = canv.width; // screen width
 	this.h = canv.height; // screen height
 	
-	this.y = 0; // background scrolling position
+	this.y = -2298; // background scrolling position
 	
 	this.padding = 10; // internal padding where player cannot step
 }
 
 Background.prototype.move = function()
 {
-	// scroll lines to the bottom a bit
+	// scroll background
 }
 	
 Background.prototype.draw = function()
 {	
-	//
+	// fill with color
+	ctx.fillStyle = 'black';
 }
 	
 Background.prototype.clear = function()
 {
 	// fill background
 	ctx.fillRect(0, 0, this.w, this.h);
+	
+	var moveY = this.y += 0.2;
+	
+	if (this.y == 0)
+	{
+		moveY = 0;
+	}
+	
 	var img = new Image();
 	
 	if (currentLevel == 1)
 	{
-	        img.src = '/game/danmaku/img/bg/fog.jpg';
+		img.src = '/game/danmaku/img/bg/mysticbg5.png';		
 	}
 	else if (currentLevel == 2)
 	{
-	        img.src = '/game/danmaku/img/bg/garden.jpg';
-	}
-	ctx.drawImage(img, 0, 0);
+		img.src = '/game/danmaku/img/bg/lotusland2.png';
+	}	
+	
+	ctx.drawImage(img, 0, moveY);
 }
 	
 Background.prototype.work = function()
